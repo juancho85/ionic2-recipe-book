6f79d540150314f1a6386738688ae877db98f47e
Juan David Sánchez <juancho85@gmail.com>
2017-01-24 21:06:13 +0100

    add firebase deependencies and initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {Platform, NavController, MenuController} from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import firebase from 'firebase';
 
 import {TabsPage} from "../pages/tabs/tabs";
 import {SigninPage} from "../pages/signin/signin";
@@ -17,6 +18,10 @@ export class MyApp {
   @ViewChild('nav') nav: NavController
   constructor(platform: Platform,
     private menuCtrl: MenuController) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBUDx5yU4oHDzCYGkhLJe2PM4z5OURF0UI",
+      authDomain: "ionic2-recipe-book-f6445.firebaseapp.com"
+    })
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
