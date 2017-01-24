0168cee991ab35c240047fef4730674dc0c54e26
Juan David Sánchez <juancho85@gmail.com>
2017-01-24 12:03:08 +0100

    display of ingredients in the shopping list

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {ShoppingListService} from "../../services/shopping-list.service";
+import {Ingredient} from "../../models/ingredient.model";
 
 @Component({
   selector: 'page-shopping-list',
@@ -8,10 +9,27 @@ import {ShoppingListService} from "../../services/shopping-list.service";
 })
 export class ShoppingListPage {
 
+
+  items: Ingredient[] = [];
+
   constructor(private shoppingListService: ShoppingListService) {}
 
   onAddItem(form: NgForm) {
-    this.shoppingListService.addItem(form.value.ingredientName, form.value.aamount);
+    this.shoppingListService.addItem(form.value.ingredientName, form.value.amount);
     form.reset();
+    this.loadItems();
+  }
+
+  ionViewWillEnter() {
+   this.loadItems();
+  }
+
+  onRemoveItem(index: number){
+    this.shoppingListService.removeItem(index);
+    this.loadItems();
+  }
+
+  private loadItems(){
+    this.items = this.shoppingListService.getItems();
   }
 }
